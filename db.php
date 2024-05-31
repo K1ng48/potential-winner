312b71fc05ca3553a8d29d13a7759e7c2fc6d85c
K1ng48 <gitk1ng48@gmail.com>
2024-06-01 00:19:52 +0300

    6

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 $dbname = "registerUser";
 
-$conn = mysqli_connect($server, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if(!$conn){
     die("Connection Fialed". mysqli_connect_error());
